8fc2d4507379c446f2c543bbd8a975f3f5479c3a
Deformator <dammandrii@gmail.com>
2017-12-06 21:33:26 -0500

    Completed view controller for Shopping list screen.

--- MAPD714-Simple-Shopping-List-App/ShoppingListItemCell.swift
+++ MAPD714-Simple-Shopping-List-App/ShoppingListItemCell.swift
@@ -1,24 +1,32 @@
-//
-//  ShoppingListItemCell.swift
-//  MAPD714-Simple-Shopping-List-App
-//
-//  Created by Andrii Damm on 2017-12-06.
-//  Copyright © 2017 Stonned Dwarfs. All rights reserved.
-//
+/*
+ * File name : ViewController.swift
+ * Author: Andrii Damm
+ * Date: December 06, 2017
+ * Student ID: 300966307
+ * Description: This class is for the custom cell of each shopping list item.
+ * Version: 0.3 - Completed view controller for Shopping list screen.
+ * Copyright © 2017 Andrii Damm. All rights reserved.
+ */
 
 import UIKit
 
+//custom cell class
 class ShoppingListItemCell: UITableViewCell {
-
+    
+    //list item name
+    @IBOutlet weak var listItem: UITextField!
+    
+    //list item's quantity
+    @IBOutlet weak var quantityPicker: UIPickerView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
--- MAPD714-Simple-Shopping-List-App/ViewController.swift
+++ MAPD714-Simple-Shopping-List-App/ViewController.swift
@@ -4,7 +4,7 @@
  * Date: December 06, 2017
  * Student ID: 300966307
  * Description: ViewController class for the shopping list screen which allows user to order items.
- * Version: 0.1 - Made view controller .
+ * Version: 0.3 - Completed view controller for Shopping list screen.
  * Copyright © 2017 Andrii Damm. All rights reserved.
  */
 
@@ -12,12 +12,29 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    private let quantity = ["1","2", "3", "4", "5", "6", "7", "9", "`10"]
+    @IBOutlet weak var tableViewShoppingList: UITableView!
+    //shopping list edit text
+    @IBOutlet weak var shoppingListName: UITextField!
     
+    //picker view array of quantity
+    private let quantity = ["0", "1","2", "3", "4", "5", "6", "7", "9", "10"]
+    
+    //identifier name for cell
+    private let shoppingListTableCellIdentifier = "shoppingItemCell"
+    
+    //number of cells in table
+    private let numberOfShoppingItems = 5
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //picker view delegate for quantity
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
+    //picker view data source for quantity
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return quantity.count
     }
@@ -26,27 +43,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return quantity[row]
     }
     
-    
-    private let shoppingListTableCellIdentifier = "shoppingItemCell"
-    private let numberOfShoppingItems = 5
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return numberOfShoppingItems
     }
     
+    // table view cell delegate
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: shoppingListTableCellIdentifier) as! ShoppingListItemCell
-//        if (cell == nil) {
-//            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: shoppingListTableCellIdentifier)
-//            
-//        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: shoppingListTableCellIdentifier) as! ShoppingListItemCell
+
         return cell
     }
+    
+    //clear all data for entire Shopping List
+    @IBAction func onCancelPressed(_ sender: UIButton) {
+        self.tableViewShoppingList.reloadData()
+    }
+    
 
 }
 
