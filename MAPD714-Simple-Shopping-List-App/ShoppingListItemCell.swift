53c3fafda72909fafe6f4c37bc23f4eee5a5578b
Deformator <dammandrii@gmail.com>
2017-12-06 20:40:19 -0500

    Adding Custom Cell

---
+++ MAPD714-Simple-Shopping-List-App/ShoppingListItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingListItemCell.swift
+//  MAPD714-Simple-Shopping-List-App
+//
+//  Created by Andrii Damm on 2017-12-06.
+//  Copyright © 2017 Stonned Dwarfs. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MAPD714-Simple-Shopping-List-App/ViewController.swift
+++ MAPD714-Simple-Shopping-List-App/ViewController.swift
@@ -1,25 +1,52 @@
-//
-//  ViewController.swift
-//  MAPD714-Simple-Shopping-List-App
-//
-//  Created by Andrii Damm on 2017-12-06.
-//  Copyright © 2017 Stonned Dwarfs. All rights reserved.
-//
+/*
+ * File name : ViewController.swift
+ * Author: Andrii Damm
+ * Date: December 06, 2017
+ * Student ID: 300966307
+ * Description: ViewController class for the shopping list screen which allows user to order items.
+ * Version: 0.1 - Made view controller .
+ * Copyright © 2017 Andrii Damm. All rights reserved.
+ */
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    private let quantity = ["1","2", "3", "4", "5", "6", "7", "9", "`10"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return quantity.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return quantity[row]
+    }
+    
+    
+    private let shoppingListTableCellIdentifier = "shoppingItemCell"
+    private let numberOfShoppingItems = 5
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfShoppingItems
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: shoppingListTableCellIdentifier) as! ShoppingListItemCell
+//        if (cell == nil) {
+//            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: shoppingListTableCellIdentifier)
+//            
+//        }
+        return cell
+    }
 
 }
 
